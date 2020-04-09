232aaa67e3d62c433e8566b38b85580018a0ef63
JorbFreire <jorbfreire@ufrn.edu.br>
2020-04-09 11:01:12 -0300

    edit repository by id

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -19,26 +19,43 @@ const repositories = [
 ];
 
 app.get("/repositories", (request, response) => {
-  return repositories;
+  return response.status(200).json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
   const { title, url, techs } = request.body;
   
-
-  repositories.push({
-    id: (repositories.length + 1),
+  const repository = {
+    id: uuid(),
     title,
     url,
     techs,
     likes: 0,
-  });
-
-  return 0;
+  };
+  
+  return response.status(200).json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+
+  const repositoryIndex = repositories.findIndex( repository => repository.id === id);
+
+  if(!title && !url && !techs)
+    return response.status(400).json({ error: "empty body" });
+  
+  if(repositoryIndex <= 0)
+    return repositoryIndex.status(403).json({ error: "Repository not found" });
+    
+  if(title)
+    repositories[repositoryIndex].title = title;
+  if(url)
+    repositories[repositoryIndex].url = url;
+  if(techs)
+    repositories[repositoryIndex].techs = techs;
+    
+  return repositories[repositoryIndex];
 });
 
 app.delete("/repositories/:id", (req, res) => {
